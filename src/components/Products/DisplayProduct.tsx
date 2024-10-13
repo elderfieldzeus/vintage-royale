bdf957586cec16025062653682a22b0266f217b6
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-10-13 22:45:53 +0800

    refactor: change svh to dvh

--- src/components/Products/DisplayProduct.tsx
+++ src/components/Products/DisplayProduct.tsx
@@ -66,7 +66,7 @@ const DisplayProduct: React.FC<IDisplayProduct> = ({showProduct, close, selected
   return (
     <>
         {showProduct && <BlackFilter full={showProduct} close={close} zLevel={20}/>}
-        <div className={`fixed z-20 bottom-0 w-full ${showProduct ? 'h-[100svh]' : 'h-0'} transition-all duration-500 rounded-t-2xl bg-white overflow-y-scroll overflow-x-hidden`}>
+        <div className={`fixed z-20 bottom-0 w-full ${showProduct ? 'h-[98dvh]' : 'h-0'} transition-all duration-500 rounded-t-2xl bg-white overflow-y-scroll overflow-x-hidden`}>
             {
                 showProduct
                 &&
@@ -77,45 +77,46 @@ const DisplayProduct: React.FC<IDisplayProduct> = ({showProduct, close, selected
             {
                 selectedProduct
                 ?
-                <div className='mb-36'>
-                    <div className='w-full aspect-square flex justify-center items-center bg-pink-50 overflow-hidden'>
-                        <img ref={imageRef} src={selectedProduct?.image_paths[imageIndex]} className='transition-all duration-500'/>
-                    </div>
-                    <div className='flex w-full gap-2 px-2 items-center h-24'>
+                <>
+                    <div className='mb-36'>
+                        <div className='w-full aspect-square flex justify-center items-center bg-pink-50 overflow-hidden'>
+                            <img ref={imageRef} src={selectedProduct?.image_paths[imageIndex]} className='transition-all duration-500'/>
+                        </div>
+                        <div className='flex w-full gap-2 px-2 items-center h-24'>
+                            {
+                                selectedProduct
+                                &&
+                                selectedProduct.image_paths.map((image, i) => {
+                                    return(
+                                        <button
+                                            type='button'
+                                            key={i}
+                                            className={`aspect-square overflow-hidden transition-all duration-500 ${i === imageIndex ? 'h-16 border-2 border-sky-300' : 'h-12'}`}
+                                            onClick={changeImage(i)}
+                                        >
+                                            <img src={image} className='w-full h-full object-cover'/>
+                                        </button>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className='font-montserrat px-2'>
+                            <p className='text-xl font-semibold leading-5'>Php {selectedProduct.price.toFixed(2)}</p>
+                            <p className='text-base'>{selectedProduct.title} | {selectedProduct.category_name}</p>
+                            <p className='text-xs'>In Stock: {selectedProduct.in_stock}</p>
+                        </div>
                         {
-                            selectedProduct
+                            selectedProduct.description
                             &&
-                            selectedProduct.image_paths.map((image, i) => {
-                                return(
-                                    <button
-                                        type='button'
-                                        key={i}
-                                        className={`aspect-square overflow-hidden transition-all duration-500 ${i === imageIndex ? 'h-16 border-2 border-sky-300' : 'h-12'}`}
-                                        onClick={changeImage(i)}
-                                    >
-                                        <img src={image} className='w-full h-full object-cover'/>
-                                    </button>
-                                )
-                            })
+                            <>
+                                <hr className='mt-2 mb-4 border-t border-gray-200' />
+                                <div className='font-montserrat px-2'>
+                                    <p className='text-xs'>Description:</p>
+                                    <p className='text-sm'>{selectedProduct.description}</p>
+                                </div>
+                            </>
                         }
                     </div>
-                    <div className='font-montserrat px-2'>
-                        <p className='text-xl font-semibold leading-5'>Php {selectedProduct.price.toFixed(2)}</p>
-                        <p className='text-base'>{selectedProduct.title} | {selectedProduct.category_name}</p>
-                        <p className='text-xs'>In Stock: {selectedProduct.in_stock}</p>
-                    </div>
-
-                    {
-                        selectedProduct.description
-                        &&
-                        <>
-                            <hr className='mt-2 mb-4 border-t border-gray-200' />
-                            <div className='font-montserrat px-2'>
-                                <p className='text-xs'>Description:</p>
-                                <p className='text-sm'>{selectedProduct.description}</p>
-                            </div>
-                        </>
-                    }
                     <div className='absolute w-full bg-white bottom-0 pt-2 pb-4 px-4 flex flex-col font-montserrat gap-2'>
                         <div className='flex justify-between items-center h-12 px-2 text-gray-400'>
                             <ChangeQuantity 
@@ -130,11 +131,11 @@ const DisplayProduct: React.FC<IDisplayProduct> = ({showProduct, close, selected
                                 isValid={quantity < selectedProduct.in_stock}
                             />
                         </div>
-                        <button type='button' className={`w-full h-12 ${quantity > 0 ? 'bg-pink-300 active:bg-pink-400' : 'bg-pink-200'} rounded-xl  text-white  transition-colors`} disabled={quantity === 0}>
+                        <button type='button' className={`w-full h-12 ${quantity > 0 ? 'bg-pink-300 active:bg-pink-400' : 'bg-pink-200'} rounded-xl text-white  transition-colors`} disabled={quantity === 0}>
                             <p>Add to Cart</p>
                         </button>
                     </div>
-                </div>
+                </>
                 :
                 <div className='w-full aspect-square flex justify-center items-center'>
                     <Loading />
